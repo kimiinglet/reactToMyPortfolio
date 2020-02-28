2f9ff5a45537da30c181a7c037a21c0f11ac3e68
kimiinglet <kinglet91391@gmail.com>
2020-02-28 05:19:38 -0600

    project 1-4 completed

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -2,7 +2,13 @@
 import React, { Component } from 'react'
 import ProjectCard from "../components/ProjectCard"
 import {Row, Col} from "react-bootstrap"
+//importing pictures from files:
 import employeeTracker from "../images/employeeTracker.png"
+import noteTaker from "../images/noteTaker.png"
+import passwordGen from "../images/passwordGen.png"
+import wanderdog from "../images/wanderdog.png"
+import fitnessTracker from "../images/fitnessTracker.png"
+import dailySchedule from "../images/dailySchedule.png"
 
 // ccc
 class Home extends Component {
@@ -14,7 +20,7 @@ class Home extends Component {
                         <Col>
                         {/*WanderDog */}
                         <ProjectCard
-                        img = {employeeTracker}
+                        img = {wanderdog}
                         name = "WanderDog"
                         website = "https://kimiinglet.github.io/meanLeanFreakGoat/"
                         github = "https://github.com/kimiinglet/meanLeanFreakGoat"
@@ -23,7 +29,10 @@ class Home extends Component {
                         <Col>
                         {/* Password Generator */}
                         <ProjectCard 
-                        project={{img:"https://via.placeholder.com/150", name:"Password Generator", website:"https://kimiinglet.github.io/pswd_gen", github:"https://github.com/kimiinglet/pswd-gen.github.io"}}
+                        img = {passwordGen}
+                        name = "Password Generator"
+                        website = "https://kimiinglet.github.io/pswd_gen"
+                        github = "https://github.com/kimiinglet/pswd-gen.github.io"
                         />
                         </Col>
                         </Row>
@@ -31,13 +40,19 @@ class Home extends Component {
                         <Col>
                         {/* Workout Tracker */}
                         <ProjectCard 
-                        project={{img: "https://via.placeholder.com/150", name: "Workout Tracker", website: "https://fitnes-trackher.herokuapp.com/", github: "https://github.com/kimiinglet/workout-tracker"}}
+                        img = {fitnessTracker}
+                        name = "Workout Tracker"
+                        website = "https://fitnes-trackher.herokuapp.com/"
+                        github = "https://github.com/kimiinglet/workout-tracker"
                         />
                         </Col>
                         <Col>
                         {/* Employee Directory */}
                         <ProjectCard 
-                            project={{img:"https://via.placeholder.com/150", name:"React Employee Directory", website:"https://react-to-employee-directory.herokuapp.com/", github:"https://github.com/kimiinglet/reactEmployeeDirectory"}}
+                        img = {employeeTracker}
+                        name = "React Employee Directory"
+                        website ="https://react-to-employee-directory.herokuapp.com/"
+                        github ="https://github.com/kimiinglet/reactEmployeeDirectory"
                         />
                         </Col>
                         </Row>
@@ -45,13 +60,19 @@ class Home extends Component {
                         <Col>
                         {/* Note Taker */}
                         <ProjectCard 
-                            project={{img:"https://via.placeholder.com/150", name:"Note Taker", website:"https://note-taker-boom.herokuapp.com/", github:"https://github.com/kimiinglet/note-taker.git"}}
+                        img = {noteTaker}
+                        name ="Note Taker"
+                        website ="https://note-taker-boom.herokuapp.com/"
+                        github ="https://github.com/kimiinglet/note-taker.git"
                         />
                         </Col>
                         <Col>
                         {/* Todo */}
                         <ProjectCard 
-                            project={{img:"https://via.placeholder.com/150", name:"ToDo", website:"https://kimiinglet.github.io./todo_list/",github:"https://github.com/kimiinglet/todo_list"}}
+                        img = "https://via.placeholder.com/150"
+                        name ="ToDo"
+                        website = "https://kimiinglet.github.io./todo_list/"
+                        github = "https://github.com/kimiinglet/todo_list"
                         />
                         </Col>
                         </Row>
@@ -59,8 +80,20 @@ class Home extends Component {
                             <Col>
                         {/* Quiz */}
                         <ProjectCard 
-                            project={{img:"https://via.placeholder.com/150", name:"Quiz", website:"https://kimiinglet.github.io/Quiz_time/.", github:"https://github.com/kimiinglet/Quiz_time.git"}}
+                        img = "https://via.placeholder.com/150"
+                        name ="Quiz"
+                        website ="https://kimiinglet.github.io/Quiz_time/."
+                        github ="https://github.com/kimiinglet/Quiz_time.git"
                         />
+                        </Col>
+                        <Col>
+                        <ProjectCard
+                        img = {dailySchedule}
+                        name ="Schedule"
+                        website ="https://kimiinglet.github.io/Quiz_time/."
+                        github ="https://github.com/kimiinglet/Quiz_time.git"
+                        />
+
                         </Col>
                         </Row>
 
