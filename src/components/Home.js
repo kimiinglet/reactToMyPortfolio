fd7039dc295393e0998a64fb19a76d95c6350d6a
kimiinglet <kinglet91391@gmail.com>
2020-03-09 23:36:16 -0500

    day planner updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import wanderdog from "../images/wanderdog-min.png"
 import fitnessTracker from "../images/fitnessTracker.png"
 import quiz from "../images/quiz.png"
 import employeeD from "../images/employeeD.png"
+import dayPlanner from "../images/dayPlanner.png"
 
 // ccc
 class Home extends Component {
@@ -32,7 +33,7 @@ class Home extends Component {
                         {/* Employee Directory */}
                         <ProjectCard 
                         img = {employeeTracker}
-                        name = "React Employee Directory"
+                        name = "Weather Dashboard"
                         website ="https://react-to-employee-directory.herokuapp.com/"
                         github ="https://github.com/kimiinglet/reactEmployeeDirectory"
                         text = "EmployeeTracker blah blah"
@@ -105,7 +106,28 @@ class Home extends Component {
                         />
                         </Col>
                         </Row>
-
+                        <Row className="projectRows">
+                        <Col>
+                        {/*employeeD */}
+                        <ProjectCard
+                        img = {employeeD} 
+                        name = "Reverse Engineering"
+                        website = "https://react-to-employee-directory.herokuapp.com/"
+                        github = "https://github.com/kimiinglet/reactEmployeeDirectory"
+                        text = "With React i have created an employee directory where you can add, delete, edit and sort all with the table directory."
+                        />
+                        </Col>
+                        <Col>
+                        {/* Employee Directory */}
+                        <ProjectCard 
+                        img = {dayPlanner}
+                        name = "Day Planner"
+                        website ="https://kimiinglet.github.io/todo_list/"
+                        github ="https://github.com/kimiinglet/todo_list.git"
+                        text = "Need to make plans? I've created a day planner from 0900-1700 that allows you to save events for each hour of the day."
+                        />
+                        </Col>
+                        </Row>
                 </div>
             
          );
