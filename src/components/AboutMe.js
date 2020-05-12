61be1880591a0756fa82a7a1bfa9ee2139ab5575
kimiinglet <kinglet91391@gmail.com>
2020-05-12 18:41:04 -0500

    autocorrect

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
                 <div id="about">
                 <h2>About Me</h2>
                 <p>Hi, Welcome, I’m Kimi. During this pandemic it has been hard for me to go out and network, so i'm going to try and do my best with letting you know who I am.</p>
-                <p>I am a very ambitious and passionate person who loves to learn new things in a way I can teach and explain to others. I recently finished a Coding Bootcamp at University of Texas at Austin for Full-Stack Web Development. I enjoy a good full stack application as well as playing around with front-end work. I find joy in helping people and learning about people which was why I was an EMT in San Francisco prior to moving back to Texas.xw
+                <p>I am a very ambitious and passionate person who loves to learn new things in a way I can teach and explain to others. I recently finished a Coding Bootcamp at University of Texas at Austin for Full-Stack Web Development. I enjoy a good full stack application as well as playing around with front-end work. I find joy in helping people and learning about people which was why I was an EMT in San Francisco prior to moving back to Texas.
                 </p>
                 <p>My little family: My partner, my dog, and two cats, enough about them... I have a heart of gold and will do anything for anyone to put a smile on their face. I’m very facetious and will pun on everything I see; it’s a hidden talent that I only show off sometimes.</p>
                 <a class="icon" href="https://www.linkedin.com/in/kimiinglet/" alt="linkedIn"><img src="https://img.icons8.com/carbon-copy/100/000000/linkedin.png" alt="linkedInIcon"/></a>
