6b68c04e8b21382a18d47a837c79cee8c921d6d5
kimiinglet <kinglet91391@gmail.com>
2020-05-12 19:10:21 -0500

    projects selected

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,16 +14,14 @@ function AboutMe() {
                 <div id="about">
                 <h2>About Me</h2>
                 <p>Hi, Welcome, I’m Kimi. During this pandemic it has been hard for me to go out and network, so i'm going to try and do my best with letting you know who I am.</p>
-                <p>I am a very ambitious and passionate person who loves to learn new things in a way I can teach and explain to others. I recently finished a Coding Bootcamp at University of Texas at Austin for Full-Stack Web Development. I enjoy a good full stack application as well as playing around with front-end work. I find joy in helping people and learning about people which was why I was an EMT in San Francisco prior to moving back to Texas.
-                </p>
-                <p>My little family: My partner, my dog, and two cats, enough about them... I have a heart of gold and will do anything for anyone to put a smile on their face. I’m very facetious and will pun on everything I see; it’s a hidden talent that I only show off sometimes.</p>
+                <p>I am a very ambitious and passionate person who loves to learn new things. I enjoy a good full stack application as well as playing around with front-end work.  I have a heart of gold and will do anything for anyone to put a smile on their face. I’m very facetious and will pun on everything I see; it’s a hidden talent that I only show off sometimes.</p>
                 <a class="icon" href="https://www.linkedin.com/in/kimiinglet/" alt="linkedIn"><img src="https://img.icons8.com/carbon-copy/100/000000/linkedin.png" alt="linkedInIcon"/></a>
                 <a class="icon" href="https://github.com/kimiinglet" alt="github"><img src="https://img.icons8.com/wired/64/000000/github.png" alt="githubIcon"/></a>
                 <a class="icon" href="https://twitter.com/IngletKimi" alt="twitter"><img src="https://img.icons8.com/carbon-copy/100/000000/twitter.png" alt="twitterIcon"/></a>
                 <br />
                 <div class="skills">Skills: HTML, Javascript, React, JQuery, AJAX, APIs, Node.js, MongoDB, Express, MySQL</div>
                 <div class="skills">Front-end: CSS, UIKit, Tailwind, Sass</div>
-                <div class="learning">Learning: Python and PHP.</div>
+                <div class="skills">Learning: Python and PHP.</div>
                 </div>
                 </Col>
             </Row>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,15 @@ import {Row, Col} from "react-bootstrap"
 
 //importing pictures from files:
 import employeeTracker from "../images/employeeTracker-min.png"
-import noteTaker from "../images/Note-taker.png"
-import passwordGen from "../images/passwordGen.png"
+// import noteTaker from "../images/Note-taker.png"
+// import passwordGen from "../images/passwordGen.png"
 import wanderdog from "../images/wanderdog-min.png"
-import fitnessTracker from "../images/fitnessTracker.png"
-import quiz from "../images/quiz.png"
+// import fitnessTracker from "../images/fitnessTracker.png"
+// import quiz from "../images/quiz.png"
 import employeeD from "../images/employeeD.png"
-import dayPlanner from "../images/dayPlanner.png"
+// import dayPlanner from "../images/dayPlanner.png"
 import BookWyrm from "../images/BookWyrm.png"
-import developer from "../images/developer.png"
+// import developer from "../images/developer.png"
 import reverse from "../images/reverse.png"
 import weather from "../images/weather.png"
 
@@ -32,7 +32,7 @@ class Home extends Component {
                         name = "Book Wyrm"
                         website = "https://book-wyrm.herokuapp.com/"
                         github = "https://github.com/kimiinglet/book-wyrm"
-                        text = "As a person who enjoys reading, I want to keep a running list of books, so that I always have something to read. "
+                        text = "Full MERN React Application. Made with React, MongoDB, Express, Tailwind, Javascript, API"
                         />
                         </Col>
                         <Col>
@@ -46,10 +46,10 @@ class Home extends Component {
                         />
                         </Col>
                         </Row>
-                        <Row className="projectRows">
+                        {/* <Row className="projectRows">
                         <Col>
                         {/* Password Generator */}
-                        <ProjectCard
+                        {/* <ProjectCard
                         img = {passwordGen}
                         name = "Password Generator"
                         website = "https://kimiinglet.github.io/pswd_gen/"
@@ -57,39 +57,17 @@ class Home extends Component {
                         text = "An application that can generate strong passwords quickly and effortlessly saves employees time and ensures secure access to data."
                         />
                         </Col>
-                        <Col>
+                        <Col> */}
                         {/* Note Taker */}
-                        <ProjectCard 
+                        {/* <ProjectCard 
                         img = {noteTaker}
                         name ="Note Taker"
                         website ="https://note-taker-boom.herokuapp.com/"
                         github ="https://github.com/kimiinglet/note-taker.git"
                         text = "An application that can be used to write, save, and delete notes. I used an express backend and save and retrieve note data from a JSON file."
                         />
-                        </Col>
-                        </Row>
-                        <Row className="projectRows">
-                        <Col>
-                        {/* Quiz */}
-                        <ProjectCard 
-                        img = {quiz}
-                        name ="Quiz"
-                        website ="https://kimiinglet.github.io/Quiz_time/."
-                        github ="https://github.com/kimiinglet/Quiz_time.git"
-                        text = "I've created a coding quiz where you are timed to answer 5 questions, and able to save your highscore to local storage."
-                        />
-                        </Col>
-                        <Col>
-                        {/* Workout Tracker */}
-                        <ProjectCard 
-                        img = {fitnessTracker}
-                        name = "Workout Tracker"
-                        website = "https://fitnes-trackher.herokuapp.com/"
-                        github = "https://github.com/kimiinglet/workout-tracker"
-                        text = "This application you're able to view create and track daily workouts."
-                        />
-                        </Col>
-                        </Row>
+                        </Col> */}
+                        {/* </Row> } */}
                         <Row className="projectRows">
                         <Col>
                         {/*employeeD */}
@@ -112,6 +90,20 @@ class Home extends Component {
                         />
                         </Col>
                         </Row>
+                        {/* <Row className="projectRows">
+                        <Col> */}
+                        {/* </Col> */}
+                        {/* <Col> */} 
+                        {/* Workout Tracker */}
+                        {/* <ProjectCard 
+                        img = {fitnessTracker}
+                        name = "Workout Tracker"
+                        website = "https://fitnes-trackher.herokuapp.com/"
+                        github = "https://github.com/kimiinglet/workout-tracker"
+                        text = "This application you're able to view create and track daily workouts."
+                        />
+÷                        </Col>
+                        </Row> */}
                         <Row className="projectRows">
                         <Col>
                         {/*WanderDog */}
@@ -121,28 +113,6 @@ class Home extends Component {
                         website = "https://kimiinglet.github.io/meanLeanFreakGoat/"
                         github = "https://github.com/kimiinglet/meanLeanFreakGoat"
                         text = "This application draws from public information to obtain free trails, camp grounds, and parks for your 4-legged pooch! Also provides weather data for your location!"
-                        />
-                        </Col>
-                        <Col>
-                        {/* Day Planner */}
-                        <ProjectCard 
-                        img = {dayPlanner}
-                        name = "Day Planner"
-                        website ="https://kimiinglet.github.io/todo_list/"
-                        github ="https://github.com/kimiinglet/todo_list.git"
-                        text = "Need to make plans? I've created a day planner from 0900-1700 that allows you to save events for each hour of the day."
-                        />
-                        </Col>
-                        </Row>
-                        <Row className="projectRows">
-                        <Col>
-                        {/*Developer Profile Generator*/}
-                        <ProjectCard
-                        img = {developer} 
-                        name = "Developer Profile Generator"
-                        website = "https://user-images.githubusercontent.com/56975796/72659024-e04c9800-397e-11ea-9b5d-562473b6103a.gif" 
-                        github = "https://github.com/kimiinglet/profile_gen"
-                        text = "This is a commond-line application that dynamically generates a PDF profile from a GitHub username. The user will be prompted for a favorite color, that will be used as the background."
                         />
                         </Col>
                         <Col>
@@ -156,6 +126,28 @@ class Home extends Component {
                         />
                         </Col>
                         </Row>
+                        {/* <Row className="projectRows">
+                        <Col>
+                        Developer Profile Generator
+                        <ProjectCard
+                        img = {developer} 
+                        name = "Developer Profile Generator"
+                        website = "https://user-images.githubusercontent.com/56975796/72659024-e04c9800-397e-11ea-9b5d-562473b6103a.gif" 
+                        github = "https://github.com/kimiinglet/profile_gen"
+                        text = "This is a commond-line application that dynamically generates a PDF profile from a GitHub username. The user will be prompted for a favorite color, that will be used as the background."
+                        />
+                        </Col> */}
+                        {/* <Col>
+                        Day Planner
+                        <ProjectCard 
+                        img = {dayPlanner}
+                        name = "Day Planner"
+                        website ="https://kimiinglet.github.io/todo_list/"
+                        github ="https://github.com/kimiinglet/todo_list.git"
+                        text = "Need to make plans? I've created a day planner from 0900-1700 that allows you to save events for each hour of the day."
+                        />
+                        </Col> */}
+                        {/* </Row> */}
             </div>
             
          );
